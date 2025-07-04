7fe4afc0b3a1c6f36474b85d4f9dd0b64b3c9270
Anastasiia Petrova <paa.front@gmail.com>
2025-07-05 00:49:19 +0300

    k

---
+++ config/config_template.py
@@ -0,0 +1,9 @@
+WEBSITES = {
+    "login_page": "https://example.com/login",
+    "admin_panel": "https://example.com/admin"
+}
+
+CREDENTIALS = {
+    "admin": {"login": "admin_example", "password": "admin123"},
+    "user": {"login": "user_example", "password": "user123"}
+}
