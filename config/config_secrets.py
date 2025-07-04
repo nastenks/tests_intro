bac98b8f0a43bd56b2d064cd33775e47ac458510
Anastasiia Petrova <paa.front@gmail.com>
2025-07-05 00:47:33 +0300

    pu

---
+++ config/config_secrets.py
@@ -0,0 +1,9 @@
+WEBSITES = {
+    "login_page": "https://real-site.com/login",  # Замените на реальный URL
+    "admin_panel": "https://real-site.com/admin"
+}
+
+CREDENTIALS = {
+    "admin": {"login": "real_admin", "password": "P@ssw0rd123"},
+    "user": {"login": "test_user", "password": "MySecret!"}
+}
